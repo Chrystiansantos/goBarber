b93670c59c3330e5fdab754bc9c217f76810c3b9
Chrystiansantos <chrystian_santos_silva@hotmail.com>
2020-04-10 22:09:56 -0300

    Layout por pagina

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import Proptypes from 'prop-types';
 
+import AuthLayout from '../pages/_layout/auth';
+import DefaultLayout from '../pages/_layout/default';
+
 export default function RouteWrapper({
   component: Component,
   isPrivate,
   ...rest
 }) {
-  const signed = false;
+  const signed = true;
 
   if (!signed && isPrivate) {
     return <Redirect to="/" />;
@@ -15,7 +18,18 @@ export default function RouteWrapper({
   if (signed && !isPrivate) {
     return <Redirect to="/dashboard" />;
   }
-  return <Route {...rest} component={Component} />;
+  const Layout = signed ? DefaultLayout : AuthLayout;
+
+  return (
+    <Route
+      {...rest}
+      render={(props) => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
+    />
+  );
 }
 
 RouteWrapper.propTypes = {
